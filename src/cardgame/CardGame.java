e8e209a08668f6b14291d8ed5f4a17b74dc91428
Dhruv Sasani <sasandhr@sheridancollege.ca>
2024-05-27 15:37:58 -0400

    Code done!

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -2,15 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package cardgame;
 
+
+package cardgame;
+import java.util.*;
 /**
  *
  * @author fagun
+ * 
  */
-
 public class CardGame {
- private static final int NUM_CARDS = 52;
+    private static final int CARDS = 52;
     private static final int HAND_SIZE = 7;
     private Card[] hand;
 
@@ -21,14 +23,14 @@ public class CardGame {
     }
 
     private void shuffleDeck() {
-        Card[] deck = new Card[NUM_CARDS];
-        for (int i = 0; i < NUM_CARDS; i++) {
+        Card[] deck = new Card[CARDS];
+        for (int i = 0; i < CARDS; i++) {
             Card.Suit suit = Card.Suit.values()[i / 13];
             Card.Value value = Card.Value.values()[i % 13];
             deck[i] = new Card(value, suit);
         }
         Random rand = new Random();
-        for (int i = NUM_CARDS - 1; i > 0; i--) {
+        for (int i = CARDS - 1; i > 0; i--) {
             int j = rand.nextInt(i + 1);
             Card temp = deck[i];
             deck[i] = deck[j];
@@ -69,7 +71,6 @@ public class CardGame {
             System.out.println("Your card is in the magic hand of random cards!");
         } else {
             System.out.println("Your card is not in the magic hand of random cards.");
-} 
+   }
+ }
 }
-}
-    
