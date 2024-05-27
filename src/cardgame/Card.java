b6686e429cb0897f6b96fb5089d7d3b5eaecfa19
sheli <sheli@10.48.16.131>
2024-05-27 13:22:25 -0400

    Committed successfully

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -10,51 +10,25 @@ package cardgame;
  * @author fagun
  */
 public class Card {
-     public enum Suit
-   {
-       HEARTS, CLUBS,SPADES, DIAMONDS
-   }
-   
-   public enum Value
-   {
-       ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
-		JACL
-   }
-    private Value value;
-    private Suit suit;
-    
-    public Card(Value v, Suit s)
-    {
-        value = v;
-        suit = s;
+
+    private final int value;
+    private final String suit;
+
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
     }
-    /**
-     * @return the value
-     */
-    public Value getValue() {
+
+    public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
-    /**
-     * @return the suit
-     */
-    public Suit getSuit() {
+    public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(Suit suit) {
-        this.suit = suit;
+    @Override
+    public String toString() {
+        return value + " of " + suit;
     }
-    
-
 }
--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,17 +4,59 @@
  */
 package cardgame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author fagun
  */
 public class CardGame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private final Card[] hand;
+    private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private static final int HAND_SIZE = 7;
+
+    public CardGame() {
+        hand = new Card[HAND_SIZE];
+        Random random = new Random();
+
+        for (int i = 0; i < HAND_SIZE; i++) {
+            int value = random.nextInt(13) + 1; // Card values from 1 to 13
+            String suit = SUITS[random.nextInt(SUITS.length)];
+            hand[i] = new Card(value, suit);
+        }
     }
-    
+
+    public void displayHand() {
+        for (Card card : hand) {
+            System.out.println(card);
+        }
+    }
+
+    public boolean searchCard(int value, String suit) {
+        for (Card card : hand) {
+            if (card.getValue() == value && card.getSuit().equalsIgnoreCase(suit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CardGame game = new CardGame();
+        game.displayHand();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a card (value and suit): ");
+        int value = scanner.nextInt();
+        String suit = scanner.next();
+
+        if (game.searchCard(value, suit)) {
+            System.out.println("Your card is in the magic hand!");
+        } else {
+            System.out.println("Your card is NOT in the magic hand.");
+        }
+    }
 }
+
