0f4d3160d275624d6383632228473584e366ce7d
arshn <arshn@10.48.11.64>
2024-05-27 16:25:48 -0400

    This code will generate 7 random cards

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -3,18 +3,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package cardgame;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author fagun
- */
 public class CardGame {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        
+      
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        
+       
+        String[] hand = new String[7];
+        
+        Random rand = new Random();
+        
+        
+        for (int i = 0; i < hand.length; i++) {
+            String card = values[rand.nextInt(values.length)] + " of " + suits[rand.nextInt(suits.length)];
+            hand[i] = card;
+        }
+        
+        
+        System.out.println("Hand of cards:");
+        for (String card : hand) {
+            System.out.println(card);
+        }
+        
+     
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a card (value and suit): ");
+        String userCard = scanner.nextLine();
+        
+        
+        boolean found = false;
+        for (String card : hand) {
+            if (card.equalsIgnoreCase(userCard)) {
+                found = true;
+                break;
+            }
+        }
+        
+       
+        if (found) {
+            System.out.println("Your card is in the hand!");
+        } else {
+            System.out.println("Your card is not in the hand.");
+        }
+        
+        scanner.close();
+    }
 }
