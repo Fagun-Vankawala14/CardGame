3c390ed3f79d45160c09e61ea82c2076d36dea62
Aayush Soni <soniaay@sheridancollege.ca>
2024-05-27 13:21:00 -0400

    InClass-Updated

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,17 +4,40 @@
  */
 package cardgame;
 
+import java.util.Random;
+
 /**
  *
  * @author fagun
  */
 public class CardGame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private String[] hand;
+
+    public CardGame() {
+        this.hand = new String[7];
+        this.dealHand();
     }
-    
+
+    private void dealHand() {
+        Random random = new Random();
+        for (int i = 0; i < 7; i++) {
+            String value = values[random.nextInt(values.length)];
+            String suit = suits[random.nextInt(suits.length)];
+            this.hand[i] = value + " of " + suit;
+        }
+    }
+
+    public void printHand() {
+        for (String card : this.hand) {
+            System.out.println(card);
+        }
+    }
+
+    public static void main(String[] args) {
+        CardGame game = new CardGame();
+        game.printHand();
+    }
 }
