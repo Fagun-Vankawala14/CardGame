d42f684f054d8fd6ed1732a7d1bb287c37842c6e
yashika <yashika2@sheridancollege.ca>
2024-05-27 13:23:55 -0400

    created an algorithm that chooses random number values and random suits from the suits array

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,9 +4,13 @@
  */
 package cardgame;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 /**
- *
- * @author fagun
+ * CardGame class models a hand of seven cards.
+ * Yashika
+ * Student Number: 991738573
  */
 public class CardGame {
 
@@ -14,7 +18,65 @@ public class CardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Card[] hand = createRandomHand();
+        displayHand(hand);
+
+        try {
+            String suitInput = JOptionPane.showInputDialog("Select a card suit (HEARTS, CLUBS, SPADES, DIAMONDS):");
+            if (suitInput == null) {
+                JOptionPane.showMessageDialog(null, "No suit entered. Exiting program.");
+                return;
+            }
+            suitInput = suitInput.toUpperCase().trim();
+
+            String valueInput = JOptionPane.showInputDialog("Select a card value (ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING):");
+            if (valueInput == null) {
+                JOptionPane.showMessageDialog(null, "No value entered. Exiting program.");
+                return;
+            }
+            valueInput = valueInput.toUpperCase().trim();
+
+            Card.Suit selectedSuit = Card.Suit.valueOf(suitInput);
+            Card.Value selectedValue = Card.Value.valueOf(valueInput);
+            Card userCard = new Card(selectedValue, selectedSuit);
+
+            boolean isCardInHand = checkCardInHand(hand, userCard);
+            if (isCardInHand) {
+                JOptionPane.showMessageDialog(null, "Your card is in the magic hand!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Your card is not in the magic hand.");
+            }
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, "Invalid suit or value. Please run the program again and enter valid card details.");
+        }
     }
-    
+
+    private static Card[] createRandomHand() {
+        Card[] hand = new Card[7];
+        Random random = new Random();
+
+        for (int i = 0; i < hand.length; i++) {
+            Card.Suit suit = Card.Suit.values()[random.nextInt(Card.Suit.values().length)];
+            Card.Value value = Card.Value.values()[random.nextInt(Card.Value.values().length)];
+            hand[i] = new Card(value, suit);
+        }
+
+        return hand;
+    }
+
+    private static void displayHand(Card[] hand) {
+        System.out.println("Magic hand:");
+        for (Card card : hand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
+    }
+
+    private static boolean checkCardInHand(Card[] hand, Card userCard) {
+        for (Card card : hand) {
+            if (card.getSuit() == userCard.getSuit() && card.getValue() == userCard.getValue()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
