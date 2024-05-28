dedf185f09faf341fcc3d4997b0ddf14d8b31731
sneh0406 <kau16126@sheridancollege.ca>
2024-05-27 21:33:55 -0400

    I have modified the CardGame code a little.

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -4,10 +4,15 @@
  */
 package cardgame;
 
+
+/* Snehdeep Kaur
+ * Student Number: 991724584
+ */
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class CardGame {
-      
     private Card[] hand;
     private static final int HAND_SIZE = 7;
     private static final Card.Suit[] suits = Card.Suit.values();
@@ -25,7 +30,7 @@ public class CardGame {
             hand[i] = new Card(value, suit);
         }
     }
-    
+
     private Card.Value getRandomValue(Random random) {
         return Card.Value.values()[random.nextInt(Card.Value.values().length - 1)];
     }
@@ -35,10 +40,34 @@ public class CardGame {
             System.out.println(card.getValue() + " of " + card.getSuit());
         }
     }
-    
+
+    public boolean containsCard(Card card) {
+        for (Card c : hand) {
+            if (c.getValue() == card.getValue() && c.getSuit() == card.getSuit()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         CardGame game = new CardGame();
         game.printHand();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of your card (e.g., ACE, KING, QUEEN): ");
+        String valueInput = scanner.nextLine().toUpperCase();
+        System.out.print("Enter the suit of your card (e.g., HEARTS, DIAMONDS, CLUBS, SPADES): ");
+        String suitInput = scanner.nextLine().toUpperCase();
+
+        Card.Value value = Card.Value.valueOf(valueInput);
+        Card.Suit suit = Card.Suit.valueOf(suitInput);
+        Card userCard = new Card(value, suit);
+
+        if (game.containsCard(userCard)) {
+            System.out.println("Your card is in the magic hand!");
+        } else {
+            System.out.println("Your card is not in the magic hand.");
+        }
     }
-    
 }
